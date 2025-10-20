1b91f746fd7d2f61a9e255423122a627193bf40c
desi-hw <desireewasszabo@gmail.com>
2025-10-20 00:57:32 -0600

    Add WHILE loop program to generate a series
    Implement a program that prompts for a positive number and prints a series based on it.

---
+++ Programa7-WHILE.c
@@ -0,0 +1,20 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Ejercicio con WHILE */
+#include <stdio.h>
+void main()
+{
+  int n;
+  do
+  {
+    printf("Dame un numero: \n");
+    scanf("%d", &n);
+  }
+  while(n<=0);
+  int num=5, s=1;
+  while(s<=n)
+  {
+    printf("El valor para la serie es: %d \n", num);
+    num=num+5;
+    s=s+1;
+  }
+}
